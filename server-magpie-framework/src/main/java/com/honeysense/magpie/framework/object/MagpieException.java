c978596ddaf3fe9e42fc965c7d533770a094f0cd
孙瑞 <smallrui@foxmail.com>
2021-03-29 06:05:19 +0800

    1、错误首字母大写。

--- server-magpie-framework/src/main/java/com/honeysense/magpie/framework/object/MagpieException.java
+++ server-magpie-framework/src/main/java/com/honeysense/magpie/framework/object/MagpieException.java
@@ -56,7 +56,16 @@ public class MagpieException extends RuntimeException {
         /**
          * 服务器内部异常
          */
-        SERVICE_UNAVAILABLE
+        SERVICE_UNAVAILABLE;
+
+        public String getClassCase() {
+            char[] chars = MagpieStringFormat.toCamelCase(name()).toCharArray();
+            if (chars[0] >= 'a' && chars[0] <= 'z') {
+                chars[0] = (char) (chars[0] - 32);
+            }
+
+            return new String(chars);
+        }
     }
 
     @ApiModelProperty(value = "异常类型", example = "NoHandlerFound", required = true)
@@ -67,7 +76,7 @@ public class MagpieException extends RuntimeException {
     private Object detail;
 
     public MagpieException(Type type) {
-        this.exception = MagpieStringFormat.toCamelCase(type.name());
+        this.exception = type.getClassCase();
 
         file = String.format("%s(%s:%s)",
                 Thread.currentThread().getStackTrace()[2].getMethodName(),
@@ -76,7 +85,7 @@ public class MagpieException extends RuntimeException {
     }
 
     public MagpieException(Type type, Object detail) {
-        this.exception = MagpieStringFormat.toCamelCase(type.name());
+        this.exception = type.getClassCase();
         this.detail = detail;
 
         file = String.format("%s(%s:%s)",
@@ -90,7 +99,7 @@ public class MagpieException extends RuntimeException {
     }
 
     public MagpieException(String exception, Object detail) {
-        this.exception = MagpieStringFormat.toCamelCase(exception);
+        this.exception = exception;
         this.detail = detail;
 
         file = String.format("%s(%s:%s)",
