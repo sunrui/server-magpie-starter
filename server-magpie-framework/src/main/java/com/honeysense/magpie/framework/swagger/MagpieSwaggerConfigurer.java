e33831f8cc2d1b714a6c83eedae2b6afef48cfaa
sunrui <smallrui@foxmail.com>
2021-12-13 21:45:36 +0800

    1、说明去不掉。

--- server-magpie-framework/src/main/java/com/honeysense/magpie/framework/swagger/MagpieSwaggerConfigurer.java
+++ server-magpie-framework/src/main/java/com/honeysense/magpie/framework/swagger/MagpieSwaggerConfigurer.java
@@ -42,7 +42,6 @@ public class MagpieSwaggerConfigurer {
                 .globalResponseMessage(RequestMethod.DELETE, responseMessageList)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.honeysense.magpie"))
-                .paths(PathSelectors.regex("^(?!/223B3143-946B-4D16-A3FB-E5495B33D1AC).*"))
                 .build();
     }
 
